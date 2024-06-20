c29f60682d0caeab371af3fde76f31ec8b309ed1
Mateus Rodrigues Da Silva <101468435+mateusrws@users.noreply.github.com>
2024-06-20 09:34:16 -0300

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,7 @@ module.exports={
         host: process.env.PGHOST,
         user: process.env.PGUSER,
         password: process.env.PGPASSWORD,
-        database: process.env.PGDATABASE
+        database: process.env.PGDATABASE,
         ssl: {
         rejectUnauthorized: false 
         }  
