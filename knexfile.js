67ac43bd95c0d863cace46c309df02e48211b348
Mateus Rodrigues Da Silva <101468435+mateusrws@users.noreply.github.com>
2024-06-20 08:23:08 -0300

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,13 @@
 const dotenv = require('dotenv').config();
 
 module.exports={
-  client:'pg',
-  connection: {
-    host: process.env.PGHOST,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE
+  development:{
+    client:'pg',
+    connection: {
+        host: process.env.PGHOST,
+        user: process.env.PGUSER,
+        password: process.env.PGPASSWORD,
+        database: process.env.PGDATABASE
+    }
   }
 }
